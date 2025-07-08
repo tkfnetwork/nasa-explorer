f0a73a431e0b22bc509502669d48a40313c4a3ff
Kris Pomphrey <kris@tkf.network>
2025-07-08 15:37:17 +0100

    chore: add ref for globe

--- clients/packages/components/src/Globe/Globe.tsx
+++ clients/packages/components/src/Globe/Globe.tsx
@@ -1,20 +1,24 @@
-import BaseGlobe from 'react-globe.gl';
-import globe from './assets/earth-blue-marble.jpg';
-import type { GlobeProps } from './Globe.types';
 import { cn } from '@/_shadcn/lib/utils';
 import { useMeasure } from '@react-hookz/web';
+import { forwardRef, RefObject } from 'react';
+import BaseGlobe from 'react-globe.gl';
+import globe from './assets/earth-blue-marble.jpg';
+import type { GlobeProps, GlobeRef } from './Globe.types';
 
-export const Globe = ({ className, ...props }: GlobeProps) => {
-  const [container, containerRef] = useMeasure<HTMLDivElement>();
+export const Globe = forwardRef<GlobeRef, GlobeProps>(
+  ({ className, ...props }, ref) => {
+    const [container, containerRef] = useMeasure<HTMLDivElement>();
 
-  return (
-    <div ref={containerRef} className={cn('w-full', 'h-full', className)}>
-      <BaseGlobe
-        globeImageUrl={globe}
-        {...props}
-        width={container?.width}
-        height={container?.height}
-      />
-    </div>
-  );
-};
+    return (
+      <div ref={containerRef} className={cn('w-full', 'h-full', className)}>
+        <BaseGlobe
+          ref={ref as RefObject<GlobeRef>}
+          globeImageUrl={globe}
+          {...props}
+          width={container?.width}
+          height={container?.height}
+        />
+      </div>
+    );
+  }
+);
--- clients/packages/components/src/Globe/Globe.types.ts
+++ clients/packages/components/src/Globe/Globe.types.ts
@@ -1,3 +1,7 @@
-import type { GlobeProps as BaseGlobeProps } from 'react-globe.gl';
+import type {
+  GlobeProps as BaseGlobeProps,
+  GlobeMethods,
+} from 'react-globe.gl';
 
 export type GlobeProps = BaseGlobeProps & { className?: string };
+export type GlobeRef = GlobeMethods;
