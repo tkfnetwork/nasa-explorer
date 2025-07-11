a96243d7e37fafdeda0509b34830f2e959e4b66e
Kris Pomphrey <kris@tkf.network>
2025-07-11 09:04:21 +0100

    chore: remove old copy/paste mock

--- clients/apps/ui/src/hocs/withProviders/withProviders.test.tsx
+++ clients/apps/ui/src/hocs/withProviders/withProviders.test.tsx
@@ -5,11 +5,7 @@ import { withProviders } from './withProviders';
 
 vi.mock('../../config/core', () => ({
   getEnv: vi.fn(() => ({
-    VITE_SANITY_PROJECT_ID: '12344567889',
-    VITE_SANITY_DATASET: '2345678998765',
-    VITE_ANJ_ID: 'ce5da088-39a1-4dea-8582-7a3d95b491f2',
-    VITE_WIDGETBOT_SERVER: '',
-    VITE_WIDGETBOT_CHANNEL: '',
+    VITE_API_BASE_URL: 'http://stub',
   })),
 }));
 
